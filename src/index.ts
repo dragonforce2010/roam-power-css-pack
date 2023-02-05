eed5a896946187042b629cf84a6ba3d1bd7b347e
michael.zhang <michael.zhang@bytedance.com>
2023-02-06 00:52:15 +0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import "./rainbow.css"
+import "./rm-bg.css"
+import "./rm-box.css"
+import "./rm-font.css"
+import "./rm-grid.css"
+import "./rm-level-font.css"
+import "./rm-level-bg.css"
+import "./global.css"
+
+const onload = () => {
+  // empty implementation for now
+}
+
+const onunload = () => {
+  // empty implementation for now
+}
+
+export default {
+  onload: onload,
+  onunload: onunload
+}
